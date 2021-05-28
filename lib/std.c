45274520d5a34ebccb1eb50e40793db8d3ad0628
deovaliandro <mdv16h@student.unhas.ac.id>
2021-05-28 21:47:37 +0800

    update for crypto

--- lib/std.c
+++ lib/std.c
@@ -26,3 +26,54 @@ int* staticInt(){
     p = (int*) malloc(sizeof(int)); 
     return p;
 }
+
+int modpow(int a, int b, int c) {
+    int dummy = a;
+
+    for (int i = 0; i < b-1; i++) {
+        dummy = (dummy * a) % c;
+    }
+
+    return dummy;
+}
+
+int isRelativePrime(int a, int b) {
+    int dummy;
+
+    while (b != 0) {
+        dummy = a % b;
+        a = b;
+        b = dummy;
+    }
+
+    return a;    
+}
+
+int isPrimitiveRoot(int prime, int root) {
+    int h[prime-2];
+    int hprime = prime;
+
+    for(int i=0; i<prime; i++) {
+        h[i] = modpow(root, i+1, prime);
+        // std::cout << h[i] << std::endl;
+    }
+    
+    int kampret = 1;
+
+    for(int i=0; i<sizeof(h)/sizeof(h[0]) - 1; i++) {
+        for (int j=kampret; j<sizeof(h)/sizeof(h[0]); j++) {
+            if (h[i] == h[j]) {
+                printf("Not primite root\n");
+                return 0;
+            }
+        }
+        
+        if(isRelativePrime(prime, h[i]) != 1) {
+            return 0;
+        }
+
+        kampret++;
+    }
+
+    return 1;
+}
--- lib/std.h
+++ lib/std.h
@@ -1,3 +1,8 @@
 void swapInt(int *p, int *q);
 int toLowerCase(char *c);
 int* staticInt();
+
+// For Cryptography
+int modpow(int a, int b, int c);
+int isRelativePrime(int a, int b);
+int isPrimitiveRoot(int prime, int root);
